a9936d11eab7e7a57a10d34cb42c53d70a88b6b2
Dikshant-Neupane <dikshantneupane690@gmail.com>
2025-07-08 17:19:26 +0545

    day-3 initial

---
+++ Days/day-3.py
@@ -0,0 +1,60 @@
+# ########  UNPACKING Tuples
+
+# fruits = ("apple", "banana", "cherry")
+
+# (green, yellow, red) = fruits
+
+# print(green)
+# print(yellow)
+# print(red)
+
+# ############## Using asterik **
+
+# fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+# (green, yellow, *red) = fruits
+
+# print(green)#gets apple as its the first 
+# print(yellow)#gets banana
+# print(red)###gets all the remaining elements
+
+
+# fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+# (green, *tropic, red) = fruits#when * is at the middle
+
+# print(green)#gets the first one
+# print(tropic)#remaining
+# # print(red)#gets the last one
+
+
+# ########## LOOPS in tuples
+# fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+# for x in fruits :
+#     print(x)###prints the output in queue
+
+# for i in range(len(fruits)):
+#     print(fruits[i])
+
+############  Using While LOOPS
+# i=0
+# while i<len(fruits):
+#     print(fruits[i])
+#     i=i+1
+
+
+################ JOINING TUPLES
+
+# tuple1 = ("a", "b" , "c")
+# tuple2 = (1, 2, 3)
+
+# tuple3 = tuple1 + tuple2
+# print(tuple3)
+
+
+# fruits = ("apple", "banana", "cherry")
+# mytuple = fruits * 2
+
+# print(mytuple)
+
+
